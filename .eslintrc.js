04c1595cef450286ca792d59f4deb0c3dc0db2f1
Isaías ramos <isaiasramosdeoliverira2003@gmail.com>
2023-01-03 13:39:52 -0300

    Addition of main folders

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    env: {
+      browser: true,
+      es2021: true,
+    },
+    extends: [
+      "plugin:react/recommended",
+      "airbnb",
+      "plugin:react/recommended",
+      "plugin:react-hooks/recommended",
+      "plugin:prettier/recommended",
+    ],
+    overrides: [],
+    parserOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    plugins: ["react"],
+    rules: {
+      "import/no-extraneous-dependencies": "off",
+    },
+  };
